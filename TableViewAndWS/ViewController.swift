97d8083df6bd605033ca7dcd84a7cd1cde012aa9
NivardoIbarra <nivardoibarra@macbook-pro-de-nivardo.local>
2015-11-03 09:46:38 -0500

    Add let :) ;)

--- TableViewAndWS/ViewController.swift
+++ TableViewAndWS/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let anyVar = "Hello Swift with Git"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
